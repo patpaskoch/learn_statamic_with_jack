2aa5edc187d45cc181c0df3739afdff48c093d0a
Patrick V T <patrick.koch.vt@gmail.com>
2025-06-12 08:16:40 +0200

    Episode 10 - Search

--- config/statamic/search.php
+++ config/statamic/search.php
@@ -28,7 +28,7 @@ return [
         'default' => [
             'driver' => 'local',
             'searchables' => 'all',
-            'fields' => ['title'],
+            'fields' => ['title', 'subtitle', 'teaser', 'post_content' ],
         ],
 
         // 'blog' => [
